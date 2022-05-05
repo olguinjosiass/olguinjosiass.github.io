e7c2ceba49c08a818398ff030b9733e1a2ab704d
olguinjosiass <93270408+olguinjosiass@users.noreply.github.com>
2022-05-05 19:10:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php 
-  session_start();
- header("location: vistas/index.php");
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>How to put PHP in HTML - Simple Example</title>
+  </head>
+  <body>
+    <h1><?php echo "This message is from server side." ?></h1>
+  </body>
+</html>
